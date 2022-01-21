b2fa35b7e0ae9e27434689032ac2f2d12b86dd7e
Piturnah <peterhebden6@gmail.com>
2022-01-21 12:07:03 +0000

    Seed generation

---
+++ Assets/Scripts/Assets.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assets : MonoBehaviour 
+{
+    public static GameObject[] EnemyPrefabs;
+    public static GameObject[] RoomObjectPrefabs;
+    public static GameObject DoorPrefab;
+}
